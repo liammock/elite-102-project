5f70da9009bee8af7d9714548d8e5b1f54628a78
Liam Mock <liammock@gmail.com>
2024-04-18 00:25:50 -0500

    LOTS of changes, but mostly pushing all my unsaved work to the repo. Project is done!

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import random
+import mysql.connector
 
-#empty list used to story user created accounts
-accounts = []
+connection = mysql.connector.connect(user = 'root', database = 'user_account', password = 'ch0psh0P!')
 
-#used as a placeholder ofr unused actions, no longer in use
+cursor = connection.cursor()
+
+"""addData = ("INSERT INTO accounts (account_id, account_name, account_value, user_email) VALUES (FLOOR(RAND() * (1000 - 1 + 1)) + 1, 'account', 3668, 'john@gmail.com')")
+
+cursor.execute(addData)
+
+testQuery = ("SELECT * FROM accounts")
+
+cursor.execute(testQuery)
+
+
+###All of this comment is just me testing out MySQL and the functions surrounding it, very useful for me to test before really coding!###
+
+
+for item in cursor:
+
+    print(item)"""
+
+
+
+
+
+#used as a placeholder for unused actions, no longer in use
 def unfinished():
   print("Currently unfinished, this is a placeholder.")
 
@@ -15,67 +37,41 @@ def display_choices():
   print("4. Help")
   print("5. Exit\n")
 
-#will ask user their username
-def ask_username():
-  username = input("Enter your username or email: ")
-  return username
-
-#will ask user their password
-def ask_password():
-  password = input("Enter your password: ")
-  return password
-
-#custom age return
-def age_finder():
-  number_is_correct = True
-  while number_is_correct is True:
-    try:
-      age = int(input("What is your age? "))
-      if age in range(1, 29):
-        print("New to this world!")
-      elif age in range(30, 59):
-        print("Same age as my younger brother!")
-      elif age in range(60, 79):
-        print("I wish I was that young!")
-      elif age >= 80:
-        print("You probably know more than me!")
-      else:
-        print("Invalid age")
-      number_is_correct = False
-      return age
-    except ValueError:
-      print("Invalid input. Please enter a valid age.")
-
-#will let you change the amount in an account
+#will let you change the amount in an account, was very simple to do since I had just completed my create account function, so I just recycled the use of using a paramater as a second execute from the cursor.execute command.
 def withdrawl_deposit():
-  which_account = 0
-  try:
-    which_account = int(input("Please enter the account number you wish to make changes to."))
-  except ValueError:
-    print("That value does not work.")
-    withdrawl_deposit()
-  for i, account in enumerate(accounts):
-    if accounts[i]["account_num"] == which_account:
-      amount = input("What would you like to set the balance to?")
-      accounts[i]["amount"] = amount
-      
-#will show you the accounts
-def view_accounts():
-  for account in accounts:
-    print("~~~~~~~~~~~~~~")
-    for key, value in account.items():
-      print(f"{key}: {value}")
-    print("~~~~~~~~~~~~~~")
+  view_accounts()
+  chosen_account_id = input("Please enter the ID of an account.")
+  chosen_value = input("What value would you like to set your account to?")
+  changeValue = ("update accounts set account_value=%s where account_id = %s")
+  valueParam = (chosen_value,chosen_account_id)
+  cursor.execute(changeValue,valueParam)
 
-#will let you create accounts
+
+#will show you the accounts, very simple and just using the code provided from the canvas page.
+def view_accounts():
+  print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+  testQuery = ("SELECT * FROM accounts")
+  cursor.execute(testQuery)
+  for item in cursor:
+    print(item)
+  print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+#will let you create accounts. Took me A LOT of trial and error, but eventually I was able to figure out (with some help from stack overflow) that f strings would not help me in this situation and I should instead use placeholders in the mySQL language and then fill them in with a second command in python, filling up the table and pushing it onto the database! I had a ton of fun and I am beginning to love coding with MySQL and python.
 def create_account():
-  type = input("Checking or savings?\n")
-  amount = 0
-  name = input("What would you like to name the account?\n")
-  new_account = Account(type,amount,name)
-  accounts.append(new_account.features())
+  user_account_id = random.randint(1,1000)
+  user_account_name = input("What would you like to name the account?")
+  initial_account_value = input("What would you like the starting balance to be?")
+  user_chosen_email = input("What email would you like to use for this account?")
+  addData = ("INSERT INTO accounts (account_id, account_name, account_value, user_email) VALUES (%s, %s, %s, %s)")
+  params = (user_account_id, user_account_name, initial_account_value, user_chosen_email)
+  cursor.execute(addData,params)
+
   
   
+  """addData = (f"INSERT INTO accounts (account_id, account_name, account_value, user_email) VALUES (FLOOR(RAND() * (1000 - 1 + 1)) + 1,{user_account_name} , {initial_account_value}, {user_chosen_email})")
+  cursor.execute(addData)"""
+
+
 
 def help():
   helping = True
@@ -103,7 +99,7 @@ def exit(talking):
   talking = False
   return talking
 
-#will display list of options
+#will display list of options. Very simple list that simply displays the options provided.
 def list_of_options(talking):
   while talking is True:
     display_choices()
@@ -124,31 +120,19 @@ def list_of_options(talking):
     except ValueError:
       print("\nI didn't understand that. Please try again.\n")
 
-#the function that runs all of the programs
+#the function that runs all of the programs. It uses a while loop to continue to show the user the menu in all of it's glory.
 def chatbot():
   talking = True
-  username = ask_username()
-  password = ask_password()
-  print(f"Hey there, {username}!")
-  age = age_finder()
+  username = input("Name?")
   print(f"\nWelcome to the Bank of Pflugerville assistant! How can I help you today, {username}?\n")
   while talking == True:
     talking = list_of_options(talking)
 
-#the class used to make accounts
-class Account:
-  def __init__(self, type, amount, name):
-    self.account_num = random.randint(1000,5000)
-    self.type = type
-    self.amount = amount
-    self.name = name
-
-  def features(self):
-    return {"account_num":self.account_num, 
-            "name":self.name,
-            "amount":self.amount, 
-            "type":self.type,
-           }
-
 
 chatbot()
+
+connection.commit()
+
+cursor.close()
+
+connection.close()
---
+++ tkinter.py
@@ -0,0 +1,9 @@
+import tkinter
+
+window = tkinter.Tk()
+label = tkinter.Label(text="Python rocks!")
+label.pack()
+
+window.mainloop()
+
+#I CANNOT GET THIS TO WORK!!! It has frustrated me greatly, but due to time constraints I will need to give up on this for now and turn in what I have...
